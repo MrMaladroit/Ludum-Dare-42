d989a33a0d59cb71fd0c81456f48d67466ed1c24
Jim Beck <jamesbeckjr@gmail.com>
2018-08-10 19:35:19 -0400

    Player Movement Created
    PlayerMovementController.cs added
    PlayerInputController.cs added

---
+++ Ludum-Dare-42/Assets/Scripts/PlayerInputController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputController : MonoBehaviour
+{
+    private PlayerMovementController movementController;
+
+    private float horizontal;
+    private float vertical;
+
+    private void Start()
+    {
+        movementController = GetComponent<PlayerMovementController>();
+    }
+
+    private void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical);
+
+        movementController.MovePlayer(direction);
+    }
+}
---
+++ Ludum-Dare-42/Assets/Scripts/PlayerMovementController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    [SerializeField]
+    private float movementSpeed = 10;
+    private Transform player;
+
+
+    private void Start()
+    {
+        player = this.gameObject.transform;
+    }
+
+    public void MovePlayer(Vector3 direction)
+    {
+        player.transform.position += direction * movementSpeed * Time.deltaTime;
+    }
+}
