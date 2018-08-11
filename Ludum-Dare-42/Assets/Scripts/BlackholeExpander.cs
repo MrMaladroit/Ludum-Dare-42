e9e510776f99c1313097426a77df8282eb728d33
Jim Beck <jamesbeckjr@gmail.com>
2018-08-10 21:50:53 -0400

    Blackhole Created
    Blackhole gameobject added
    BlackholeCollision.cs added
    BlackholeExpander.cs added
    Blackhole.psd added

---
+++ Ludum-Dare-42/Assets/Scripts/BlackholeExpander.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackholeExpander : MonoBehaviour
+{
+    [SerializeField]
+    private float growthRate = 0.002f;
+
+    private Transform blackholeGraphic;
+
+    private void Start()
+    {
+        blackholeGraphic = GetComponentInChildren<Transform>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 blackholeScale = new Vector3(growthRate, growthRate);
+        blackholeGraphic.transform.localScale += blackholeScale;
+    }
+}
